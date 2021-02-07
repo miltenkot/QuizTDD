981274ea7171a35e99bf20dfc72bf3531439e8b6
miltenkot <miltenkot@gmail.com>
2021-02-07 12:11:15 +0100

    Add @escaping closures and retain cycles

--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -16,34 +16,63 @@ class FlowTest: XCTestCase {
         
         sut.start()
         
-        XCTAssertEqual(router.routedQuestionCount, 0)
+        XCTAssertTrue(router.routedQuestions.isEmpty)
     }
     
-    func test_start_withNoQuestions_routesToQuestion() {
+    func test_start_withOneQuestions_routesToCorrectQuestion() {
         let router = RouterSpy()
         let sut = Flow(questions: ["Q1"], router: router)
         
         sut.start()
         
-        XCTAssertEqual(router.routedQuestionCount, 1)
+        XCTAssertEqual(router.routedQuestions, ["Q1"])
     }
     
-    func test_start_withNoQuestions_routesToCorrectQuestion() {
+    func test_start_withOneQuestions_routesToCorrectQuestion2() {
         let router = RouterSpy()
-        let sut = Flow(questions: ["Q1"], router: router)
+        let sut = Flow(questions: ["Q2"], router: router)
         
         sut.start()
         
-        XCTAssertEqual(router.routedQuestion, "Q1")
+        XCTAssertEqual(router.routedQuestions, ["Q2"])
+    }
+    
+    func test_start_withTwoQuestions_routesToCorrectQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+        
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestions, ["Q1"])
+    }
+    
+    func test_startTwice_withTwoQuestions_routesToCorrectQuestionTwice() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+
+        sut.start()
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestions, ["Q1","Q1"])
+    }
+    
+    func test_startAndAnswerFirst_withTwoQuestions_routesToSecondQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1","Q2"], router: router)
+        
+        sut.start()
+        
+        router.answerCallback("A1")
+        
+        XCTAssertEqual(router.routedQuestions, ["Q1", "Q2"])
     }
     
     class RouterSpy: Router {
-        var routedQuestionCount: Int = 0
-        var routedQuestion: String? = nil
-        
-        func routeTo(question: String) {
-            routedQuestionCount += 1
-            routedQuestion = question
+        var routedQuestions: [String] = []
+        var answerCallback: ((String) -> Void) = {_ in}
+        func routeTo(question: String, answerCallback: @escaping (String)->Void) {
+            routedQuestions.append(question)
+            self.answerCallback = answerCallback
         }
         
       
